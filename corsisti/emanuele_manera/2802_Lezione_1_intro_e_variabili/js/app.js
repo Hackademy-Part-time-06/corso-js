27a9e90ed82086cf0f5ac321a396a4ee8f93ca56
Emanuele Manera <122477176+emanuelemanera@users.noreply.github.com>
2023-02-28 21:02:46 +0100

    28/02 - Lezione 1 - intro e variabili

---
+++ corsisti/emanuele_manera/2802_Lezione_1_intro_e_variabili/js/app.js
@@ -0,0 +1,44 @@
+// Esercizio 1
+
+var movieTitle = "Pulp fiction"; 
+var movieYear = 1994;
+var movieDirector = "Quentin Tarantino"; 
+
+console.log(movieTitle);
+console.log(movieYear);
+console.log(movieDirector); 
+
+console.log(movieTitle, '('+movieYear, '-', movieDirector+')')
+
+// Esercizio 2 
+
+var bookTitle 
+bookTitle = "Il Piccolo Principe"; 
+
+var bookDescription = "Un aviatore racconta...";
+
+console.log(bookTitle);
+console.log(bookDescription);
+
+console.log(bookDescription, 'la storia de', bookTitle)
+
+// Esercizio 3 
+
+var admin 
+var name 
+
+name = "Gino"
+admin = name 
+
+// console.log(admin)
+// console.log(name)
+
+// alert(admin)
+
+//Esercizio 4 - BONUS
+
+// var username = prompt('inserisci username')
+// var password = prompt('inserisci password')
+
+// alert('Benvenuto '+username+' nell"area personale.') 
+
