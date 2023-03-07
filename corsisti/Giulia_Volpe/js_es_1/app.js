c3e4aa6523cd04867145fe0ee4cb50e9a66d0712
giulia volpe <giuliavolpe@hotmail.it>
2023-03-07 10:33:42 +0100

    Caricamento esercitazione 1

---
+++ corsisti/Giulia_Volpe/js_es_1/app.js
@@ -0,0 +1,32 @@
+//ESERCIZIO 1
+/*var movieTitle = "Pulp Fiction";
+var movieYear = "1994";
+var movieDirector = "Quentin Tarantino"
+
+console.log("Movie:", movieTitle);
+console.log("Year:", movieYear);
+console.log("Director:", movieDirector);
+
+console.log(movieTitle + " ( "  +  movieYear + "-" + movieDirector +")");
+
+//ESERCIZIO 2
+
+var bookTitle;
+bookTitle = "La collina dei conigli";
+var bookDescription = " parla di sopravvivenza nel branco";
+console.log(bookTitle + bookDescription);
+
+//ESERCIZIO 3
+
+var admin;
+var name;
+name = "Tobia";
+admin = name;
+//alert(admin);
+
+//ESERCIZIO 4
+
+let username = prompt("inserisci username")
+let password = prompt("inserisci password")
+alert("benvenuto "+username)
+*/
