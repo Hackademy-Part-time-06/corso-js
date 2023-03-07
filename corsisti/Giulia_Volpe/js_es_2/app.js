135d283cd014832e39b2773581ada11a9a338dec
giulia volpe <giuliavolpe@hotmail.it>
2023-03-07 10:36:57 +0100

    Caricata esercitazione 2

---
+++ corsisti/Giulia_Volpe/js_es_2/app.js
@@ -0,0 +1,86 @@
+//ESERCIZIO 8
+/*
+console.log("La somma è: "+(10+20+35+50+70));
+console.log("La media è: "+((10+20+35+50+70)/5));
+*/
+
+//ESERCIZIO 9
+/*
+let num1 = prompt ("Insersci un numero");
+let num2 = prompt ("Insersci un numero");
+let num3 = prompt ("Insersci un numero");
+let num4 = prompt ("Insersci un numero");
+let num5 = prompt ("Insersci un numero");
+num1 = +num1;
+num2 = +num2;
+num3 = +num3;
+num4 = +num4;
+num5 = +num5;
+console.log("La somma è: "+(num1+num2+num3+num4+num5));
+console.log(("La media è: "+(num1+num2+num3+num4+num5)/5));
+*/
+/*
+// ESERCIZIO 10
+
+let thisYear = 2023;
+let birthYear = 1998;
+let maxAge = 100;
+console.log("Io ho", thisYear - birthYear, "anni");
+console.log("mi mancano", maxAge - (thisYear-birthYear) + " anni per arrivare a " + maxAge);
+*/
+//ESERCIZIO 11
+/*
+let name = prompt("Inserisci il tuo nome");
+let nameCapitalized = name.charAt(0).toUpperCase()+name.slice(1);
+console.log("Il mio nome è: " +nameCapitalized);
+*/
+
+// ESERCIZIO 12
+/*
+let numeroTel = prompt("inserisci il tuo numero");
+console.log("*******"+ (numeroTel.slice("7", "10")));
+*/
+
+//ESERCIZIO 13
+
+/*
+let smallNumb = Math.min(23, 2, 31, 74, 95);
+let largeNumb = Math.max(23, 2, 31, 74, 95);
+console.log("smallest number is: " + smallNumb);
+console.log("largest number is: " + largeNumb);
+*/
+
+//ESERCIZIO 14
+/*
+let num1 = 0.1;
+let num2 = 0.2;
+let somma = (num1+num2).toFixed(1);
+console.log("La somma è: " +somma);
+*/
+
+
+//ESERCIZIO 15
+/*
+console.log("Numero randomico: " +Math.floor(Math.random() *7));
+*/
+
+//ESERCIZIO 16
+/*
+const totalCats = 44;
+const catsPerRow = 6;
+let catsRows = Math.round(totalCats/catsPerRow);
+console.log(catsRows);
+let module = totalCats%catsPerRow;
+console.log(module)
+let missingCats = catsPerRow-module;
+console.log(missingCats);
+*/
+
+/*
+let File= Math.round(Gatti/GattiFila)
+console.log(File)
+let modulo= Gatti % GattiFila
+console.log(modulo)
+let GattiMancanti= GattiFila-modulo
+console.log(GattiMancanti)
+*/
