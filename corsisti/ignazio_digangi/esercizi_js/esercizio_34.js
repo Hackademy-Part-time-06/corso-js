4654468dc3ba072860dca949acec8232b0ea06f8
ignazio di gangi <ignazio.dgangi@gmail.com>
2023-03-15 18:47:01 +0100

    soluzione esercizi

---
+++ corsisti/ignazio_digangi/esercizi_js/esercizio_34.js
@@ -0,0 +1,24 @@
+console.log("Esercizio 34")
+console.log("Simula una partita a dadi")
+/*
+Scrivi una funzione che si occupi di lanciare i dadi (usa la funzione creata nell’esercizio precedente) un numero di volte che verrà definito tramite parametro passato in input.
+Una volta lanciati i dadi controlla chi ha vinto (vince chi lancia il numero più alto).
+*/
+
+let numeroLanci =prompt("Quanti sono i lanci:");
+let numero;
+
+function lanciaDadi(){
+    let giocatore1 = 0;
+    let giocatore2 = 0;
+
+    for (let i = 1; i <= numeroLanci; i++){
+        giocatore1 += Math.round(Math.random() * 6);
+        giocatore2 += Math.round(Math.random() * 6);
+        
+    }
+    console.log();
+    console.log();
+}
+
+lanciaDadi();
