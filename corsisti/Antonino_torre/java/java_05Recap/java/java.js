8e3db20d556491b6fabf70658c344c972cbe3c66
Tony <torretony94@gmail.com>
2023-03-25 21:23:10 +0100

    aggiunto esercizio fine settimana

--- corsisti/Antonino_torre/java/java_05Recap/java/java.js
+++ corsisti/Antonino_torre/java/java_05Recap/java/java.js
@@ -166,3 +166,194 @@ function partita (){
 }
 partita ()
 */
+// esercizio 69
+/*
+let persone = [
+    {
+            nome: "Gino",
+            cognome: "Ginoli",
+            nuovoUtente: false,
+    },
+    {
+            nome: "Pino",
+            cognome: "Pinoli",
+            nuovoUtente: true,
+    }
+]
+let lista = document.createElement("ul")
+lista.setAttribute("class","list-group")
+document.body.append(lista)
+persone.forEach(function (personaSingola){
+    let datiUtente = document.createElement("li")
+    datiUtente.setAttribute("class","list-group-item")
+    lista.append(datiUtente)
+    for (let dato in personaSingola){
+        datiUtente.innerText=personaSingola.nome + " " + personaSingola.cognome +"  " 
+    }
+    if (personaSingola.nuovoUtente === true){
+        let badge = document.createElement("span")
+        badge.setAttribute("class", "badge bg-secondary")
+        badge.innerText = "NEEEEW!"
+        datiUtente.append(badge)
+        }
+})
+*/
+// esercizio 70 
+/*
+let articoli = [
+    {
+            titolo: "Il realismo magico di Sergio Toppi",
+            riassunto: `NPE ristampa una storia del cosiddetto "Filone fantastico toppiano", creata dal maestro Sergio Toppi nel 1987: "Isola Gentile".`,
+            link: `https://www.lospaziobianco.it/il-realismo-magico-di-sergio-toppi/`,
+            immagine: `https://www.lospaziobianco.it/wp-content/uploads/2023/02/ImmagineEvidenza2-240x150.jpg`
+    },
+    {
+            titolo: `Ma Gesù ascolta musica Punk? “Punk Rock Jesus” di Sean Murphy`,
+            riassunto:`"Punk Rock Jesus" di Sean Murphy è un concentrato di azione ipercinetica, personaggi dai tratti estremi e satira caciarona dei costumi e della religione.`,
+            link: `https://www.lospaziobianco.it/ma-gesu-ascolta-musica-punk-punk-rock-jesus-di-sean-murphy/`,
+            immagine: `https://www.lospaziobianco.it/wp-content/uploads/2023/03/Sean-Murphy-punk-rock-jesu-240x150.jpg`
+    },
+    {
+            titolo: `La Zona Fantasma: i nuovi incubi Junji Ito`,
+            riassunto: `Una nuova raccolta di racconti recentissimi per il maestro dell’orrore giapponese nati per il web.`,
+            link: `https://www.lospaziobianco.it/la-zona-fantasma-i-nuovi-incubi-junji-ito/`,
+            immagine: `https://www.lospaziobianco.it/wp-content/uploads/2023/02/genkai-chitai-junji-ito-bien-e1675344852796-340x195.jpg`
+    }
+]
+let row = document.createElement("div")
+row.setAttribute("class","row")
+row.classList.add("w-75")
+document.body.append(row)
+articoli.forEach(function(album){
+    let colonne = document.createElement("div")
+    row.append(colonne)
+    colonne.setAttribute("class","col-4")
+    let card = document.createElement("div")
+    colonne.append(card)
+    card.setAttribute("class","card")
+    let immagine = document.createElement("img")
+    card.append(immagine)
+    let body= document.createElement("div")
+    body.setAttribute("class","card-body")
+    card.append(body)
+    let titolo = document.createElement("h5")
+    body.append(titolo)
+    let riassunto = document.createElement("p")
+    body.append(riassunto)
+    let link = document.createElement("a")
+    body.append(link)
+    link.setAttribute("class","btn btn-primary")
+    link.innerText = "Leggi di più"
+    for (let key in album){
+        immagine.setAttribute("src",album.immagine)
+        titolo.innerText = album.titolo
+        riassunto.innerText = album.riassunto
+        link.setAttribute("href",album.link)
+    }
+})
+*/
+//esercizio 71
+/*
+function register(){
+    nome = prompt("inserisci nome")
+    email = prompt("Inserisci la tua mail")
+    password= prompt("inserisci una password valida")
+    let contenitore= document.createElement("div")
+    document.body.append(contenitore)
+    let modale= `
+    <div class="modal fade" id="modale-registrazione" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        Ciao ${nome} (${email}), benvenuto nella tua area personale 
+        </div>
+        <div class="modal-footer">
+        </div>
+      </div>
+    </div>
+  </div>`
+  contenitore.innerHTML= modale
+  const modaleRegistrazione = new bootstrap.Modal(document.getElementById('modale-registrazione'))
+  modaleRegistrazione.show()
+}
+register()
+*/
+// esercizio 72
+/*
+let lanciodeldado = document.createElement("div")
+document.body.append(lanciodeldado)
+lanciodeldado.innerHTML= `<div id="dado"></div>
+<button id="lancia-dado">Lancia dado</button>
+`
+let bottone = document.getElementById("lancia-dado")
+let testoDado= document.getElementById("dado")
+
+bottone.addEventListener("click",function(){
+    testoDado.innerText= Math.floor(Math.random() * ( 6 - 1 ) + 1)
+})
+*/
+// esercizio 73
+/*
+let link = document.createElement("span")
+link.innerHTML= `<a href="https://www.google.it/"">Cerca su google</a>`
+document.body.append(link)
+let click= 0
+function cliccastaminchia(event){
+    event.preventDefault()
+    alert("Sfigato, non abbandonare la pagina, per favore")
+    click = click + 1
+    console.log(click)
+    if (click>1){
+        link.removeEventListener("click",cliccastaminchia)
+    }
+}
+
+link.addEventListener("click",cliccastaminchia)
+*/
+// esercizio 74
+/*
+const LIBRI = [
+    {
+            titolo: "Il Signore degli Anelli",
+            autore: "Tolkien",
+            categoria: "fantasy"
+    },
+    {
+            titolo: "Harry Potter",
+            autore: "Rowling",
+            categoria: "fantasy",
+    },
+    {
+            titolo: "Il Conte di Montecristo",
+            autore: "Dumas",
+            categoria: "narrativa classica"
+    },
+    {
+            titolo: "Dune",
+            autore: "Herbert",
+            categoria: "fantascienza"
+    },
+    {
+            titolo: "Fight Club",
+            autore: "Palahniuk",
+            categoria: "narrativa moderna"
+    }
+]
+let lista = document.createElement("ul")
+document.body.append(lista)
+LIBRI.forEach(function(listeLibri){
+    let libro = document.createElement("li")
+    lista.append(libro)
+    bottone= document.createElement("button")
+    bottone.innerHTML = "Aggiungi ai preferiti"
+    lista.append(bottone)
+    libro.innerHTML = `<strong>Titolo: </strong>${listeLibri.titolo}  <br><strong>Autore: </strong>${listeLibri.autore} <br><strong>Categoria: </strong>${listeLibri.categoria}`
+    bottone.addEventListener("click",function(){
+        alert(`Il libro "${listeLibri.titolo}" è stato aggiunto ai preferiti`)
+    })
+})
+*/
