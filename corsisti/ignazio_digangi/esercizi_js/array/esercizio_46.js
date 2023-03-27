297e3813afdcb1e65e97a5de62acf3581a143aa4
ignazio di gangi <ignazio.dgangi@gmail.com>
2023-03-27 16:40:50 +0200

    creazione esercizi dal 37 al 48

---
+++ corsisti/ignazio_digangi/esercizi_js/array/esercizio_46.js
@@ -0,0 +1,9 @@
+"use strict"
+console.log("Esercizio 46")
+console.log("File JS caricato correttamente")
+/*
+Scrivi una funzione che, data una frase, ritorni la prima lettera di ogni parola presente nella frase:
+es: 
+input: “Ciao sono un esercizio sui metodi avanzati degli array” 
+output: Csuesmada
+*/
---
+++ corsisti/ignazio_digangi/esercizi_js/array/esercizio_47.js
@@ -0,0 +1,20 @@
+"use strict"
+console.log("Esercizio 47")
+console.log("File JS caricato correttamente")
+/*
+Scrivi una funzione crei un unico array unendo gli array innestati
+   es:
+     input:
+     [
+
+         ["abc", "gino", "aaa"],
+         ["abc", "dino", 123],
+         ["aaa", "gino", 321],
+         ["xyz", "klm", "aaa"],
+
+     ]
+     
+     output
+
+        ["abc", "gino", "aaa", "abc", "dino", 123, "aaa", "gino", 321, "xyz", "klm", "aaa”]
+*/
