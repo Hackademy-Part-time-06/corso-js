3a37b92684d4ac377e710c6a0314009ff1921133
ignazio di gangi <ignazio.dgangi@gmail.com>
2023-03-27 16:13:20 +0200

    creazione esercizio 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16

---
+++ corsisti/ignazio_digangi/esercizi_js/flowchart_tipi/esercizio_10.js
@@ -0,0 +1,10 @@
+"use strict"
+console.log("File JS caricato correttamente");
+
+/*
+Scrivi un programma che dato l'anno corrente e un anno di nascita determini: 
+
+- l'età della persona
+- quanti anni sono necessari per raggiungere i 100
+- restituisca in output (console.log o alert) entrambi i risultati.
+*/
---
+++ corsisti/ignazio_digangi/esercizi_js/flowchart_tipi/esercizio_12.js
@@ -0,0 +1,6 @@
+"use strict"
+console.log("File JS caricato correttamente");
+
+/*
+Scrivi un programma che dato un numero di telefono (di 10 cifre) in input, offuschi tutte le cifre, sostituendole con l'asterisco (*), e lasci visibili solo le ultime 3.
+*/
