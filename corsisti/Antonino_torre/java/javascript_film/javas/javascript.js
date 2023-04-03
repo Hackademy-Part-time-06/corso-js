6e92b94e88293f17fd1754522365fab0883a7fa1
Tony <torretony94@gmail.com>
2023-04-04 00:38:07 +0200

    finito esercizio 87

---
+++ corsisti/Antonino_torre/java/javascript_film/javas/javascript.js
@@ -0,0 +1,35 @@
+"use strict"
+
+//http://www.omdbapi.com/?apikey=[yourkey]&
+let urlsito = new URL(`http://www.omdbapi.com/`)
+urlsito.searchParams.append("apikey","a25de3ce")
+
+let inputFilm = document.getElementById("cerca_film")
+let bottoneCerca = document.getElementById("button-addon2")
+bottoneCerca.addEventListener("click",function(event){
+    urlsito.searchParams.append("s",inputFilm.value)
+    fetch(urlsito)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(raccolta){
+        let contenitore = document.createElement("div")
+        contenitore.setAttribute("class", "row")
+        document.body.append(contenitore)
+        raccolta.Search.forEach(function(film){
+            let card = document.createElement("div")
+            card.setAttribute("class","col-3")
+            contenitore.append(card)
+            card.innerHTML= `<div class="card h-100">
+              <img src="${film.Poster}" onError="this.onerror=null;this.src='https://placehold.co/600x400?text=image+not+present';" class="card-img-top" alt="Poster">
+              <div class="card-body">
+                <h5 class="card-title">${film.Title}</h5>
+                <div class="card-text">${film.Year}</div>
+                <div class="card-text">${film.Type}</div>
+                <div class="card-text"><a href="http://127.0.0.1:5500/film.html?imdbID=${film.imdbID
+            }&" target="_blank">Vai alla scheda</a></div>
+              </div>
+            </div>`
+        })
+    })
+})
