18b2858698565d70800d001d18f62935f3477b0f
Alberto Cuogo <cuogoalberto@gmail.com>
2023-04-08 10:28:55 +0200

    add Selfwork JS10, JS11, JS12, JS13, JS14

---
+++ corsisti/alberto_cuogo/JS14_Selfwork/Cookie Banner/js/adv.js
@@ -0,0 +1,9 @@
+"use strict"
+
+console.log("[ADV] pubblicità caricata")
+try {
+    analytics.registraAzione("erogazione pubblicità");
+}
+catch(e) {
+    console.error("[ADV] errore durante il richiamo dell'analytics")
+}
