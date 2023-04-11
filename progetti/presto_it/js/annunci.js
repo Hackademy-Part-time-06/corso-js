b46e56ada6dec4c67e6bcc34a1624b400e26bb85
niloaulab <nicola.loddo@aulab.it>
2023-04-11 20:21:15 +0200

    presto.it: un pò di layout + chiamata agli annunci

--- progetti/presto_it/js/annunci.js
+++ progetti/presto_it/js/annunci.js
@@ -1,3 +1,16 @@
 "use strict"
 
 console.log("annunci.js caricato")
+
+function  caricaAnnunci() {
+    fetch("/progetti/presto_it/fake-server/api/annunci.json")
+    .then(response => response.json())
+    .then(listaAnnunci => {
+        console.log("Lista annunci:", listaAnnunci);
+    })
+    .catch((error) => {
+        console.error("Errore nella chiamata all'api degli annunci:", error)
+    })}
+
+
+caricaAnnunci();
