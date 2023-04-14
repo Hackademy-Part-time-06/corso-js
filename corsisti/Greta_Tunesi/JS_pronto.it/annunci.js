5d82cbf6adc89c0cceac639dfd5f2868a28a9fea
Aterg998 <greta.tunesi@forestvalley.org>
2023-04-14 20:17:28 +0200

    ho fatto finalmente le card con il badge

--- corsisti/Greta_Tunesi/JS_pronto.it/annunci.js
+++ corsisti/Greta_Tunesi/JS_pronto.it/annunci.js
@@ -2,18 +2,26 @@
 
 console.log("file JS annunci caricato")
 
-let contenitoreListaAnnunci = document.getElementById("contanitore-lista-annunci")
+let contenitoreListaAnnunci = document.getElementById("contenitore-lista-annunci")
+
+let listaAnnunciGlobale = []
 
 function caricaAnnunci() {
-
     fetch("/corsisti/Greta_Tunesi/JS_pronto.it/fake-server/annunci.json")
-        .then(response => response.json())
-        .then((listaAnnunci) => {
-            console.log(listaAnnunci)
+    .then(response => response.json())
+    .then((listaAnnunci) => {
+        listaAnnunciGlobale = listaAnnunci
+        //abbiamo creato una variabile globale perchè sennò questa variabile 
+        //esisterebbe solo dentro questa funzione
+        //e dandole il valore di una variabile globale la possiamo usare
+        //perchè no il return dici? perchè non si usa con le robe asincrone
+    console.log("lista annunci", listaAnnunci)
 
-            stampaListaAnnunci()
-
+    stampaListaAnnunci(listaAnnunci)
         })
+    .catch((error) => {
+        console.error("Errore nella chiamata all'api degli annunci:", error)
+    })
 }
 caricaAnnunci()
 
@@ -21,6 +29,16 @@ caricaAnnunci()
 function stampaCardAnnunci(annuncio) {
     let contenitoreSingolaCard = document.createElement("div")
     contenitoreSingolaCard.classList.add("col-12", "col-sm-4", "mb-4");
+
+
+    let badgeEl;
+    if (annuncio.type === "sell") {
+        badgeEl = ` <span class="badge bg-danger annuncio-type-badge">Vendo</span>`;
+    }
+    else {
+        badgeEl = ` <span class="badge bg-custom annuncio-type-badge">Cerco</span>`;
+    }
+
     contenitoreSingolaCard.innerHTML =
         `<div class="card">
     <img src="https://placehold.co/600x400?text=image+not+present" class="card-img-top">
@@ -35,8 +53,10 @@ function stampaCardAnnunci(annuncio) {
         <div class="text-center w-50 py-2">${annuncio.created_date}</div>
     </div>
     </div>`
-}
 
+    contenitoreListaAnnunci.append(contenitoreSingolaCard)
+    
+}
 
 
 function stampaListaAnnunci(listaAnnunci=[]) {
