1d9b54d2565ec9ee401c0e26259e1c77b8d3a153
giulia volpe <giuliavolpe@hotmail.it>
2023-04-17 10:47:34 +0200

    presto_it: push fino a filtri

---
+++ corsisti/Giulia_Volpe/Presto_it/JavaScript/common.js
@@ -0,0 +1,58 @@
+"use strict"
+
+//VARIABILI GLOBALI
+let menuSelect = document.getElementById("select-categoria")
+
+function currentPage() {
+    let menuEl
+    if (window.location.href.includes("index.html")) {
+        console.log("Sono nella HOME");
+        menuEl = document.getElementById("menu-home");
+    }
+    else if (window.location.href.includes("annunci.html")) {
+        console.log("Sono nella sezione ANNUNCI");
+        menuEl = document.getElementById("menu-annunci")
+    }
+    if (menuEl) {
+        menuEl.classList.add("active")
+    }
+}
+
+function dropCategorie() {
+    fetch("/MockApi/categorie.json")
+        .then(response => response.json())
+        .then(categorie => {
+            console.log(categorie)
+            let ulCat = document.getElementById("dd-menu-cat")
+
+            categorie.forEach(categoria => {
+                let liCateg = document.createElement("li")
+                liCateg.innerHTML = `<a class="dropdown-item" href="#">${categoria.name}</a>`
+                ulCat.append(liCateg)
+            });
+
+        })
+        .catch(error => {
+            console.log("Oooooops... Qualcosa è andato storto", error)
+        })
+}
+
+function selectCategoria() {
+    if (menuSelect) {
+        fetch("/MockApi/categorie.json")
+        .then(response => response.json())
+        .then(listaCategorie => {
+            console.log("lista categorie:", listaCategorie)
+            listaCategorie.forEach(categoria =>{
+                let optionSelect = document.createElement("option");
+                optionSelect.innerText = categoria.name
+                menuSelect.append(optionSelect)
+            })
+        })
+    }
+}
+
+
+currentPage()
+dropCategorie()
+selectCategoria()
---
+++ corsisti/Giulia_Volpe/Presto_it/JavaScript/index.js
@@ -0,0 +1,29 @@
+"use strict"
+
+// VARIABILI GLOBALI 
+
+let btnSearch = document.getElementById("btn-cerca");
+let input = document.getElementById("inputSearch");
+let selectPerCategoria = document.getElementById("select-categoria")
+let prezzoMin = document.getElementById("cerca-prezzo-min")
+let prezzoMax = document.getElementById("cerca-prezzo-max")
+
+
+function cercaPerNome() {
+    let queryInput = input.value;
+
+    let url = new URL(window.location.origin + "/annunci.html")
+    url.searchParams.set("query", queryInput)
+    window.location = url.href;
+    console.log("input cerca---", queryInput)
+}
+
+//LISTENERS
+
+function listenSearch() {
+    btnSearch.addEventListener("click", event => {
+        cercaPerNome()
+    })
+}
+
+listenSearch()
